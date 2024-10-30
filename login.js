dd65c482ce940091b3cb67e16f6f02a6c0a3e06f
Marcos-DGalarza <marcos.g3464@gmail.com>
2024-10-30 01:46:48 -0300

    Update login.js agregacion de validaciones

--- login.js
+++ login.js
@@ -1,24 +1,50 @@
 // Credenciales de ejemplo (por el momento simulo los datos de un usuario en lugar de obtenerlos de una base de datos)
-const validEmail = "usuario@gmail.com";
-const validPassword = "contraseña123";
+const email = "usuario@gmail.com";
+const password = "contraseña123";
 
 const form = document.getElementById('formulario');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevenimos el envío del formulario por el momento
+document.getElementById("email").addEventListener("input", validarEmail);
+document.getElementById("password").addEventListener("input", validarUsuario);
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    
-    if (email === validEmail && password === validPassword) {
-        form.reset();
-        document.getElementById('mensaje-error').classList.remove('mensaje_error-activo');
-        document.getElementById('mensaje-exito').classList.add('mensaje_exito-activo');
-        setTimeout(() => {
-            document.getElementById('mensaje-exito').classList.remove('mensaje_exito-activo');
-        }, 3000); // a los 3 segundos se elimina el mensaje
+function validarEmail() {
+    const input_email = document.getElementById("email").value;
+    const validarSintaxis = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validarSintaxis.test(input_email)) { // valida que el email
+        document.getElementById("error-email").innerText = "El email debe contener un @ y un dominio válido.";
+        document.getElementById("error-email").style.display = "block";
+        return false;
     } else {
-        document.getElementById('mensaje-error').classList.add('mensaje_error-activo');
+        document.getElementById("error-email").style.display = "none";
+        return true;
+    }
+}
+
+function validarUsuario() {
+    const input_password = document.getElementById("password").value;
+    const input_email = document.getElementById("email").value;
+    if (input_password !== password || input_email !== email) { // valida que el email y la contraseña ya esten registrados
+        return false;
+    } else {
+        return true;
+    }
+}
+
+form.addEventListener('submit', function(evento) {
+    evento.preventDefault();// Prevenimos el envío del formulario por el momento
+    
+    if (validarUsuario() && validarUsuario()) {
+        document.getElementById("error-login").style.display = "none";
+        document.getElementById("exito-login").style.display = "block";
+        document.getElementById("exito-login").innerText = "Inicio de Sesion exitoso!.";
+        setTimeout(() => {
+            document.getElementById("exito-login").style.display = "none";
+        }, 5000); // a los 5 segundos se elimina el mensaje de exito
+        form.reset(); // simula el envio del formulario
+    } else {
+        document.getElementById("exito-login").style.display = "none";
+        document.getElementById("error-login").style.display = "block";
+        document.getElementById("error-login").innerText = "Error: Email y/o contraseña incorrectos.";
+        form.reset();
     }
 });
